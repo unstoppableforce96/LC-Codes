e80bd1663955d8928d7802dbf0e1f4c999e6a587
Pavan B <55431858+unstoppableforce96@users.noreply.github.com>
2026-08-06 10:30:34 +0530

    Time: 41 ms (71.31%) | Memory: 29.7 MB (92.95%) - LeetSync

--- 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
+++ 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
@@ -1,15 +1,14 @@
 class Solution:
     def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
-        currentSum = 0
-        left = 0
-        ans = 0
-        for right in range(len(arr)):
-            currentSum += arr[right]
-            if right >= k - 1:
-                avg = currentSum / k
-                if avg >= threshold:
-                    ans += 1
-                # Subtracting the left most value from sum
-                currentSum -= arr[left]
-                left += 1
-        return ans
+        first_window = arr[:k]
+        currentSum = sum(first_window)
+        count = 0
+        if currentSum / k >= threshold:
+            count += 1
+        for i in range(k, len(arr)):
+            # add a new element to the sum
+            # subtract the old element
+            currentSum = currentSum + arr[i] - arr[i - k]
+            if currentSum / k >= threshold:
+                count += 1
+        return count
