7c8b0e35ca97b76d7a2cc7e1239e64429d959154
Pavan B <55431858+unstoppableforce96@users.noreply.github.com>
2026-08-06 22:02:55 +0530

    Time: 128 ms (38.77%) | Memory: 37.2 MB (41.42%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -3,14 +3,16 @@ class Solution:
         d = {}
         currentSum = 0
         maximumSum = 0
+        left = 0
         for right in range(len(nums)):
             currentSum += nums[right]
             d[nums[right]] = d.get(nums[right], 0) + 1
-            if right >= k - 1:
-                if len(d) == k:
-                    maximumSum = max(maximumSum, currentSum)
-                d[nums[right - k + 1]] -= 1
-                currentSum -= nums[right - k + 1]
-                if d[nums[right - k + 1]] == 0:
-                    d.pop(nums[right - k + 1])
+            if right - left > k - 1:
+                currentSum -= nums[left]
+                d[nums[left]] -= 1
+                if d[nums[left]] == 0:
+                    d.pop(nums[left])
+                left += 1
+            if len(d) == k:
+                maximumSum = max(currentSum, maximumSum)
         return maximumSum
