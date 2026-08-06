d765ce3c411d3f9784bff8bb9c6cba6c559d461c
Pavan B <55431858+unstoppableforce96@users.noreply.github.com>
2026-08-06 22:17:08 +0530

    Time: 102 ms (76.09%) | Memory: 37.2 MB (66.50%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -1,18 +1,32 @@
 class Solution:
     def maximumSubarraySum(self, nums: List[int], k: int) -> int:
+        # Process the first window of size k
         d = {}
         currentSum = 0
         maximumSum = 0
-        left = 0
-        for right in range(len(nums)):
-            currentSum += nums[right]
-            d[nums[right]] = d.get(nums[right], 0) + 1
-            if right - left > k - 1:
-                currentSum -= nums[left]
-                d[nums[left]] -= 1
-                if d[nums[left]] == 0:
-                    d.pop(nums[left])
-                left += 1
+        for i in range(k):
+            currentSum += nums[i]
+            d[nums[i]] = d.get(nums[i], 0) + 1
+        if len(d) == k:
+            maximumSum = max(currentSum, maximumSum)
+
+        # Process the rest of the array, by adding new element
+        # and removing the left most element since every new addition
+        # from here causes the window to grow to k + 1 and to keep
+        # it at size k, we must kick out the left most element
+        for i in range(k, len(nums)):
+            # add new element
+            currentSum += nums[i]
+            d[nums[i]] = d.get(nums[i], 0) + 1
+
+            # kick out the left most, which is at i - k
+            out = nums[i - k]
+            currentSum -= out
+            d[out] -= 1
+            if d[out] == 0:
+                d.pop(out)
+
+            # Check the maxSum again
             if len(d) == k:
                 maximumSum = max(currentSum, maximumSum)
         return maximumSum
