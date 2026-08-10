4f163419f5cf39d72b445ef363a9eee2e988dbc6
Pavan B <55431858+unstoppableforce96@users.noreply.github.com>
2026-08-10 19:51:42 +0530

    Time: 178 ms (100.00%) | Memory: 35.1 MB (100.00%) - LeetSync

--- 4363-minimum-total-price-after-applying-discounts/minimum-total-price-after-applying-discounts.py
+++ 4363-minimum-total-price-after-applying-discounts/minimum-total-price-after-applying-discounts.py
@@ -1,13 +1,14 @@
 class Solution:
     def minPrice(self, prices: list[int], discounts: list[int]) -> float:
-        prices.sort(reverse=True)
-        discounts.sort(reverse=True)
         n = len(prices)
         m = len(discounts)
+        prices.sort(reverse=True)
+        discounts.sort(reverse=True)
+        k = min(n, m)
+        ans = sum(prices[k:])
         i = j = 0
-        final_price = 0
         while i < n and j < m:
-            final_price += prices[i] * (1 - discounts[j] / 100)
+            ans += prices[i] * (1 - discounts[j] / 100)
             i += 1
             j += 1
-        return final_price + sum(prices[i:])
+        return ans
