718f410565f761820216fe9010a895cb10a59928
Pavan B <55431858+unstoppableforce96@users.noreply.github.com>
2026-08-11 23:37:49 +0530

    Time: 122 ms (5.19%) | Memory: 25.3 MB (50.23%) - LeetSync

--- 525-contiguous-array/contiguous-array.py
+++ 525-contiguous-array/contiguous-array.py
@@ -3,11 +3,11 @@ class Solution:
         for i in range(len(nums)):
             if nums[i] == 0: nums[i] = -1
         ans = 0
-        seen = {0: [-1]}
+        seen = {0: -1}
         current_sum = 0
         for i in range(len(nums)):
             current_sum += nums[i]
             if current_sum in seen:
-                ans = max(ans, i - min(seen[current_sum]))
-            seen[current_sum] = seen.get(current_sum, []) + [i]
+                ans = max(ans, i - seen[current_sum])
+            seen[current_sum] = seen.get(current_sum, i)
         return ans
