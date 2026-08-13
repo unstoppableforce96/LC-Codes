df19920d7d772e443822333dc66648a134d9eea3
Pavan B <55431858+unstoppableforce96@users.noreply.github.com>
2026-08-13 20:36:29 +0530

    Time: 1 ms (97.53%) | Memory: 47.1 MB (89.14%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -2,16 +2,17 @@ class Solution {
     public int pivotIndex(int[] nums) {
         int n = nums.length;
         // Compute the prefix array
-        int[] prefix = new int[n + 1];
+        // int[] prefix = new int[n + 1];
+        int totalSum = 0;
         for (int i = 0; i < n; i++) {
-            prefix[i + 1] = prefix[i] + nums[i];
+            totalSum += nums[i];
         }
+        int runningSum = 0;
         for (int i = 0; i < n; i++) {
-            int leftSum = prefix[i];
-            int rightSum = prefix[n] - prefix[i + 1];
-            if (leftSum == rightSum) {
+            if (runningSum == totalSum - (runningSum + nums[i])) {
                 return i;
             }
+            runningSum += nums[i]; // Basically left sum here
         }
         return -1;
     }
