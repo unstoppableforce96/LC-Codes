a9c7cc72122373b3add01a3979653fd8d29f241f
Pavan B <55431858+unstoppableforce96@users.noreply.github.com>
2026-08-16 23:30:15 +0530

    Time: 147 ms (25.42%) | Memory: 19.8 MB (15.37%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -5,7 +5,7 @@ class Solution:
         maxWindow = 0
         for right in range(len(s)):
             seen[s[right]] = seen.get(s[right], 0) + 1
-            while seen.values() and sum(seen.values()) - max(seen.values()) > k:
+            while (right - left + 1) - max(seen.values()) > k:
                 seen[s[left]] -= 1
                 left += 1
             maxWindow = max(maxWindow, right - left + 1)
