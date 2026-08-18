95c682bf7457e8ec0fdd048352b58c06185e9d6e
Pavan B <55431858+unstoppableforce96@users.noreply.github.com>
2026-08-18 22:32:00 +0530

    Time: 142 ms (11.55%) | Memory: 145.2 MB (5.27%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
@@ -1,17 +1,30 @@
 class NumMatrix {
     int[][] matrix;
+    int[][] prefix;
     public NumMatrix(int[][] matrix) {
         this.matrix = matrix;
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        this.prefix = new int[rows + 1][cols + 1];
+        // Building prefix arrays
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                int top = prefix[i][j + 1];
+                int left = prefix[i + 1][j];
+                int overlap = prefix[i][j];
+                int current = matrix[i][j];
+                prefix[i + 1][j + 1] = top + left - overlap + current;
+            }
+        }
+        System.out.println(Arrays.deepToString(prefix));
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        int sum = 0;
-        for (int i = row1; i <= row2; i++) {
-            for (int j = col1; j <= col2; j++) {
-                sum += this.matrix[i][j];
-            }
-        }
-        return sum;
+        int whole = this.prefix[row2 + 1][col2 + 1];
+        int topRemove = this.prefix[row1][col2 + 1];
+        int leftRemove = this.prefix[row2 + 1][col1];
+        int overlap = this.prefix[row1][col1];
+        return whole - topRemove - leftRemove + overlap;
     }
 }
 
