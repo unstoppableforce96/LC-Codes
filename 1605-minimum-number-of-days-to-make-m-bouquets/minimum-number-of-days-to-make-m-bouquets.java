80223ee9b566ba752e711bbe2746417187c7281e
Pavan B <55431858+unstoppableforce96@users.noreply.github.com>
2026-08-20 21:25:07 +0530

    Time: 18 ms (50.20%) | Memory: 82.3 MB (92.15%) - LeetSync

---
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -0,0 +1,46 @@
+class Solution {
+    public static boolean isBloomed(int day, int curDay) {
+        return day <= curDay;
+    }
+    public static boolean isPossible(int[] bloomDay, int m, int k, int curDay) {
+        int cons = 0;
+        for (int i = 0; i < bloomDay.length; i++) {
+            if (isBloomed(bloomDay[i], curDay)) {
+                cons++;
+                if (cons == k) {
+                    m--;
+                    cons = 0;
+                }
+            }
+            else {
+                cons = 0;
+            }
+        }
+        return m <= 0;
+    }
+    public static int maxBloomDay(int[] bloomDay) {
+        int mx = bloomDay[0];
+        for (int day: bloomDay) {
+            mx = Math.max(day, mx);
+        }
+        return mx;
+    }
+    public int minDays(int[] bloomDay, int m, int k) {
+        if (bloomDay.length < 1l * m * k) {
+            return -1;
+        }
+        int low = 1;
+        int high = maxBloomDay(bloomDay);
+        int ans = Integer.MAX_VALUE;
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (isPossible(bloomDay, m, k, mid)) {
+                high = mid;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+}
