91a02f8f79945f637931270794c9817ec42a4ff7
Pavan B <55431858+unstoppableforce96@users.noreply.github.com>
2026-08-20 19:58:33 +0530

    Time: 12 ms (27.63%) | Memory: 50.3 MB (20.10%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,46 @@
+class Solution {
+    public static boolean canShip(int weights[], int shipWeight, int daysHave) {
+        int daysNeeded = 0;
+        int tSumSoFar = 0;
+        for (int weight: weights) {
+            if (tSumSoFar + weight <= shipWeight) {
+                tSumSoFar += weight;
+            }
+            else {
+                daysNeeded++;
+                tSumSoFar = weight;
+            }
+        }
+        if (tSumSoFar != 0) {
+            daysNeeded++;
+        }
+        return daysNeeded <= daysHave;
+    }
+    public static int getMax(int[] weights) {
+        int maxWeight = weights[0];
+        for (int weight: weights) {
+            maxWeight = Math.max(weight, maxWeight);
+        }
+        return maxWeight;
+    }
+    public static int getSum(int[] weights) {
+        int sum = 0;
+        for (int weight: weights) {
+            sum += weight;
+        }
+        return sum;
+    }
+    public int shipWithinDays(int[] weights, int days) {
+        int low = getMax(weights), high = getSum(weights);
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (canShip(weights, mid, days)) {
+                high = mid;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+}
