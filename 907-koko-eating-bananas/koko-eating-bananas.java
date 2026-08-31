ab2073d2ab7838ef9e6eb1aec0e64891207dc94e
Pavan B <55431858+unstoppableforce96@users.noreply.github.com>
2026-08-31 22:43:57 +0530

    Time: 22 ms (25.27%) | Memory: 48.5 MB (10.99%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,32 +1,32 @@
 class Solution {
-
+    public static boolean canEat(int[] piles, int hoursHave, int k) {
+        // Compute the hours needed to eat all piles at k bananas / per hour speed
+        long hoursNeeded = 0;
+        for (int pile: piles) {
+            hoursNeeded += (int)Math.ceil(1d * pile / k);
+        }
+        return hoursNeeded <= hoursHave;
+    }
+    public static int getMax(int[] piles) {
+        int max = piles[0];
+        for (int i = 0; i < piles.length; i++) {
+            if (piles[i] > max) max = piles[i];
+        }
+        return max;
+    }
     public int minEatingSpeed(int[] piles, int h) {
-
-        int n = piles.length;
-        long total = 0;
-
-        for (int num : piles) {
-            total += num;
-        }
-
-        int start = (int) ((total + h -1) / h);
-        int end = (int) ((total - 2*n + h + 1) / (h - n + 1));
-
-        while (start < end) {
-            int mid = start + (end - start) / 2;
-            int time = 0;
-
-            for (int num : piles) {
-                time += (int) ((num + mid - 1) / mid) ;
+        // Binary Search on Answer
+        int low = 1;
+        int high = getMax(piles); // max(piles)
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (canEat(piles, h, mid)) {
+                high = mid;
             }
-
-            if (time > h) {
-                start = mid + 1;
-            } else {
-                end = mid;
+            else {
+                low = mid + 1;
             }
         }
-
-        return start;
+        return low;
     }
 }
