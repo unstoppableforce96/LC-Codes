9e1b44bfb5ae7ba674d5c488fe28e0fd30b707bb
Pavan B <55431858+unstoppableforce96@users.noreply.github.com>
2026-09-20 14:51:01 +0530

    Time: 166 ms (21.89%) | Memory: 31.4 MB (48.38%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -1,29 +1,23 @@
 class Solution:
-    def largestRectangleArea(self, heights: List[int]) -> int:
+    def largestRectangleArea(self, heights: list[int]) -> int:
         n = len(heights)
+        nse = [n] * n
+        pse = [-1] * n
         st = []
-        max_area = 0
-        for i in range(n + 1):
-            current_height = 0 if i == n else heights[i]
-            while st and current_height < heights[st[-1]]:
-
-                # found NSE -> heights[i] is the NSE of st[-1]
-                right_smaller = i  
-
-                # pop st top
-                bar = st.pop()
-
-                # Now whatever the element is on stack top is the Left smaller
-                if st:
-                    left_smaller = st[-1]
-                else:
-                    left_smaller = -1
-                
-                # Calculate area
-                area = (right_smaller - left_smaller - 1) * heights[bar]
-                max_area = max(area, max_area)
+        for i in range(len(heights)):
+            while st and heights[i] < heights[st[-1]]:
+                nse[st.pop()] = i
             st.append(i)
-        print(st)
+        st.clear()
+        for i in range(len(heights) - 1, -1, -1):
+            while st and heights[i] < heights[st[-1]]:
+                pse[st.pop()] = i
+            st.append(i)
+        max_area = 0
+        for i in range(n):
+            width = nse[i] - pse[i] - 1
+            height = heights[i]
+            area = width * height
+            max_area = max(area, max_area)
         return max_area
 
-                
